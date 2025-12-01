4dc1bc132fb6687a68ce5b45a0b7e85bab70a16e
Egor Novozhilov <e.novozhilov@docsinbox.ru>
2025-12-01 23:50:24 +0100

    feat: init

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-};
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages не поддерживает оптимизацию изображений
+  },
+  basePath: '/rufi_bd_25', // Замените на имя вашего репозитория
+  assetPrefix: '/rufi_bd_25/',
+}
 
 export default nextConfig;
